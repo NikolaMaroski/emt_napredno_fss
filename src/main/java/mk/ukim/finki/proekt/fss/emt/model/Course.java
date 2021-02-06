3ba76649e4fd11f330b7994f04ee69e6f787ae6c
Nikola Maroski <nikolamaroski@hotmail.com>
2021-02-06 22:03:46 +0100

    Mapping the entities

--- src/main/java/mk/ukim/finki/proekt/fss/emt/model/Course.java
+++ src/main/java/mk/ukim/finki/proekt/fss/emt/model/Course.java
@@ -1,4 +1,22 @@
 package mk.ukim.finki.proekt.fss.emt.model;
+import lombok.Data;
 
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+@Data
+@Table(name = "course")
 public class Course {
+
+    @Id
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "courses")
+    private List<Student> students;
+
+    @ManyToOne
+    private StudyProgram studyProgram;
+
 }
