1eb47012a38e0d1bd8a4ee5591e2a63b522c8213
Nikola Maroski <nikolamaroski@hotmail.com>
2021-02-06 21:41:17 +0100

    Initial project with dependencies

---
+++ src/main/java/mk/ukim/finki/proekt/fss/emt/EmtApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.proekt.fss.emt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmtApplication.class, args);
+    }
+
+}
